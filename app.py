ede7ee54f146f14ac3a184eb389a0138a7fca55a
jatingarg <jatingarg2596@gmail.com>
2025-10-25 21:20:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -170,5 +170,4 @@ def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host="0.0.0.0", port=port)
+      app.run(debug=True)
